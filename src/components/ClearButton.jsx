abd0b39a2008f51da3d6a5afffd9f61341593a21
Wag1Sasha <4goenko@gmail.com>
2020-03-05 16:46:35 +0200

    project

---
+++ src/components/ClearButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ClearButton.css';
+
+
+
+class ClearButton extends React.Component{
+    render(){
+        return(
+            <div className='clearButton' onClick={()=>this.props.handleClear()}>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+export default ClearButton;
